2d583e2985fa2ecd1e07793d4e001d80581b8db8
Ely Karr <elykarr@gmail.com>
2021-10-19 10:52:07 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -19,23 +19,52 @@ fetch("http://localhost:3000/toys")
   .then(data => init(data))
 
 function init(toys) {
-  toys.forEach(element => () {
-    renderToys(toy);
+  toys.forEach(toy => {
+    console.log(toy)
+    renderToy(toy);
   });
+  const createToyBttn = document.querySelector(".submit")
+  createToyBttn.addEventListener("submit", createToy)
 }
 
 const toyCollectionDiv = document.querySelector("#toy-collection");
-const addToyDiv = document.createElement("div");
 
 
-function renderToys(toys) {
+function renderToy(toy) {
+  const addToyDiv = document.createElement("div");
   addToyDiv.className = "card";
-  addToyDiv.id = `${toy.name}`
 
-  addToyDiv.append()
+  const img = document.createElement("img");
+  img.className = "toy-avatar"
+  img.src = toy.image
+
+  const toyName = document.createElement("h2");
+  toyName.innerText = toy.name
+
+  const likesNum = document.createElement("h5");
+  likesNum.className = "likes-num"
+  likesNum.textContent = toy.likes
+
+  p = document.createElement("p");
+  p.innerText = "Likes"
+
+  const likesBttn = document.createElement("button");
+  likesBttn.className = "like-btn"
+  likesBttn.innerText = "Like"
+  likesBttn.addEventListener("click", () => increaseLikes(toy, likesNum))
+
+
+  addToyDiv.append(img, toyName, p, likesNum, likesBttn)
   toyCollectionDiv.appendChild(addToyDiv)
 
+}
+
+function createToy(event) {
 
 
 }
 
+function increaseLikes(toy, likesNum) {
+  ++toy.likes;
+  likesNum.textContent = toy.likes
+}
